ffa06cce7d7f5e4e900987ce54a08370bc5e10a2
Preston Elliott <preston.elliott24@gmail.com>
2019-03-02 16:10:38 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(x)
+  x = 11 
+  while x > 1
+    x -= 1 
+    puts "#{x} SECOND(S)!"
+  end
+  x = 0 
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(x)
+  x = 11 
+  while x > 1
+    x -= 1 
+    sleep(1)
+    puts "#{x} SECOND(S)!"
+  end
+  x = 0 
+  return "HAPPY NEW YEAR!"
 end
